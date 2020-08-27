acbdfc9ad565a008421172d98585bbef24faa366
Wolle <26044260+schreibfaul1@users.noreply.github.com>
2020-08-27 18:31:58 +0200

    Add files via upload

--- a2dp_source.cpp
+++ a2dp_source.cpp
@@ -91,7 +91,7 @@ void bt_app_task_handler(void *arg){
 //---------------------------------------------------------------------------------------------------------------------
 void bt_app_task_start_up(void)
 {
-    s_bt_app_task_queue = xQueueCreate(10, sizeof(bt_app_msg_t));
+    s_bt_app_task_queue = xQueueCreate(20, sizeof(bt_app_msg_t));
     xTaskCreate(bt_app_task_handler, "BtAppT", 2048, NULL, configMAX_PRIORITIES - 3, &s_bt_app_task_handle);
     return;
 }
